2f417c0fe04ef46e9a7f2ae17e4375b09a503194
yhetlaw <jandresantos14@gmail.com>
2021-10-06 16:51:42 +0100

    3.3 finished

--- index.js
+++ index.js
@@ -30,12 +30,25 @@ app.get('/api/persons', (req, res) => {
   res.json(persons);
 });
 
+//res.send(`Insert id of person to show <input id="input-id"></input>`);
+
 app.get('/info', (req, res) => {
   let a = new Date().toString();
   console.log(a);
   res.send(`<p>Phonebook has info for ${persons.length} people</p>
   <p>${a}</p>
   `);
+});
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find((person) => person.id === id);
+
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
 });
 
 const PORT = 3001;
