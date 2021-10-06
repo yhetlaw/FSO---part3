07cb190a9bd75fe29fa03c4279b9f813e1640b75
yhetlaw <jandresantos14@gmail.com>
2021-10-06 15:29:52 +0100

    3.2 finished

--- index.js
+++ index.js
@@ -30,6 +30,14 @@ app.get('/api/persons', (req, res) => {
   res.json(persons);
 });
 
+app.get('/info', (req, res) => {
+  let a = new Date().toString();
+  console.log(a);
+  res.send(`<p>Phonebook has info for ${persons.length} people</p>
+  <p>${a}</p>
+  `);
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
