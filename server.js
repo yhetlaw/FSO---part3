f372bc565287f6708e3d3212dc73e342c3e2dc2a
yhetlaw <jandresantos14@gmail.com>
2021-10-14 17:58:29 +0100

    3.9 finished

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const { json } = require('express');
 const express = require('express');
 const morgan = require('morgan');
+
 const app = express();
 app.use(express.json());
+
 morgan.token('body', (req) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
+
+const cors = require('cors');
+app.use(cors());
 
 let persons = [
   {
