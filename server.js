4ff33a272d7434016baec09b7915ea715ee57707
yhetlaw <jandresantos14@gmail.com>
2021-10-19 14:56:41 +0100

    3.11 unfinished

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 
 const app = express();
 app.use(express.json());
+app.use(express.static('build'));
 
 morgan.token('body', (req) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
